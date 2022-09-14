9dff42b72ec1f9ea3118518d68f238b6dd138ecf
DrexNanat <nanatdrex@gmail.com>
2022-09-14 12:58:52 -0700

    commiting

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,13 +7,14 @@
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		return (1);
+		if (c == i)
+		{
+			return (1);
+		}
 	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+	return (0);
 }
