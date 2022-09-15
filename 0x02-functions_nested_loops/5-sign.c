17eec6a0e318324f95b959f03b7311c2c415fd6f
DrexNanat <nanatdrex@gmail.com>
2022-09-15 13:57:50 -0700

    5-print_numbe

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,29 +4,21 @@
  * Return: Always 0.
  */
 
-int main(void)
+int print_sign(int n)
 {
-	int r;
-
-	r= print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		return (-1);
+	}
 }
